e898c362bb4bc567da45944d09ee884750fc5def
Brynn <brynnreginajohnson@gmail.com>
2021-02-24 15:53:29 -0600

    fix user action variable names

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -1,4 +1,4 @@
-import * as UserAPIUtil from '../util/user_api_util';
+import * as UserApiUtil from '../util/user_api_util';
 
 export const RECEIVE_USER = "RECEIVE_USER";
 // export const RECEIVE_USER_ERRORS = "RECEIVE_USER_ERRORS";
