16972483e1ad96ec1ea01f5b0b5e23771cb8866f
Tului <tgantulga@ucdavis.edu>
2021-02-25 15:27:18 -0800

    trying to figure out how to get info from database

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -1,15 +1,21 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// const IngredientSchema = new Schema( {
-//     users : [{
-//         type: Schema.Types.ObjectId,
-//         ref: 'users'
-//     }],
-//     name: {
+const IngredientSchema = new Schema( {
+    
+  id: {
+    type: Number,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  }
+})
 
-//     },
-//     rating: {
 
-//     }
-// })
+module.exports = Ingredient = mongoose.model('Ingredient', IngredientSchema);
