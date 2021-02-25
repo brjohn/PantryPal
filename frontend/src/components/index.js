f94e0f8657450da80ad9106919f27ef4a0115d59
droid4alex <droid4alex@gmail.com>
2021-02-25 14:59:07 -0800

    Updates index.js for /recipe route

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -8,6 +8,7 @@ import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import SearchContainer from './search/search_container';
 import PantryContainer from './pantry/pantry_container';
+import RecipeConstainer from './recipe/recipe_container';
 
 const App = () => (
   <div>
@@ -19,6 +20,7 @@ const App = () => (
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
       <ProtectedRoute exact path="/pantry" component={PantryContainer} />
+      <ProtectedRoute exact path="/recipe" component={RecipeConstainer} />
     </Switch>
   </div>
 );
