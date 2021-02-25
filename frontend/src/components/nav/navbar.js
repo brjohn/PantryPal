1e1327fbef0e528d07045e5c8ffd3b29fcc5f0cc
colewendling <colewendling@utexas.edu>
2021-02-24 18:33:50 -0600

    move modal css file

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import './navbar.css'
-import './modal.css'
+import '../modal/modal.css'
 
 class NavBar extends React.Component {
   constructor(props) {
