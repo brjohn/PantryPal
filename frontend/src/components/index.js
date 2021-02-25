32157bcb0aab7c2de91c457b9b3dc51695a809ea
colewendling <colewendling@utexas.edu>
2021-02-25 14:30:37 -0600

    add pantry and fix routes

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -7,17 +7,18 @@ import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import SearchContainer from './search/search_container';
-
+import PantryContainer from './pantry/pantry_container';
 
 const App = () => (
   <div>
     <Modal />
     <NavBarContainer />
-    <SearchContainer/>
+    {/* <SearchContainer /> */}
     <Switch>
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      <ProtectedRoute exact path="/pantry" component={PantryContainer} />
     </Switch>
   </div>
 );
