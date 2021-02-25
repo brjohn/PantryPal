89179d1a4da06b288bd9e9cda78113d0ec4ec7ad
droid4alex <droid4alex@gmail.com>
2021-02-24 18:04:52 -0800

    Adds spoonacular_api_util.js, recipe_actions.js, recipe_search_container.js, recipe_search.js

---
+++ frontend/src/components/recipe/recipe_search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class RecipeSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: '',
+      recipes: ''
+    };
+  }
+
+  render() {
+  }
+}
+
+export default RecipeSearch;
