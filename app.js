b97198d6a496913f1fc64f3107b91be6456fcfd6
Tului <tgantulga@ucdavis.edu>
2021-02-25 17:21:22 -0800

    ingredients.js is able to get ingredient from database

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const users = require("./routes/api/users");
+const ingredients = require("./routes/api/ingredients");
 
 // app.get("/", (req, res) => res.send("Helloooooooooooooooo World"));
 
@@ -28,6 +29,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use("/api/users", users);
+app.use("/api/ingredients", ingredients);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- routes/api/ingredients.js
+++ routes/api/ingredients.js
@@ -1,27 +1,21 @@
 const express = require("express");
 const router = express.Router();
-// const bcrypt = require('bcryptjs');
+
 const Ingredient = require('../../models/Ingredient');
-// const jwt = require('jsonwebtoken');
-// const keys = require('../../config/keys');
-// const passport = require('passport');
+
+
+router.get('/', (req, res) => {
+    console.log(req)
+
+    // this works
+    Ingredient.findOne({ name: req.body.name})
+        .then(ingredientInfo => res.json(ingredientInfo))
+        .catch(err => console.log(err))
+// 
+
+})
 
 
 
-
-router.get('/:name', (req, res) => {
-    Ingredient.findOne({name: req.params.name}) // this shoudl be by name
-        .then(ingredient => res.json(ingredient))
-        .catch(err =>
-            res.status(404).json({ noingredientfound: 'No ingredient found with that ID' })
-        );
-});
-
-// Ingredient.findOne({ name: 'apple' })
-//     .then(res => console.log(res))
-//     .catch(err => console.log(err))
-
-
-// module.exports = router;
-
+module.exports = router;
 
