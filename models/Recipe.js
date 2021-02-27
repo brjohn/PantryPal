3d2408479f48286f7c42548145d109142af3ea8b
droid4alex <droid4alex@gmail.com>
2021-02-27 12:00:58 -0800

    Updated recipe schema for storing recipe objects in our db

--- models/Recipe.js
+++ models/Recipe.js
@@ -2,18 +2,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const RecipeSchema = new Schema({
-  user_id: {
-    type: Schema.Types.ObjectId,
-    ref: "users",
-  },
-  spoontacular_recipe_id: {
-    type: Number,
-    required: true
-  },
-  rating: {
-    type: Number,
+  // user_id: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "users",
+  // },
+  // spoonacularId: {
+  //   type: Number,
+  //   required: true
+  // },
+  // title: {
+  //   type: String,
+  //   required: true,
+  // },
+  // image: {
+  //   type: String,
+  //   required: true,
+  // },
+  // usedIngredients: {
+  //   type: String,
+  //   required: true,
+  // },
+  recipeObject: {
+    type: Object,  //test this to make sure we can store multiple objects in Recipe schema
     required: true,
   },
 }, {
   timestamps: true,
 })
+
+
+// id, title, image, usedIngredients, directions
