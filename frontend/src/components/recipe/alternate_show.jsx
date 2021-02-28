ee625fd966aa2fe79c70159f44bdb57cd446d072
Brynn <brynnreginajohnson@gmail.com>
2021-02-28 17:09:12 -0600

    remove old show, add image to new show

--- frontend/src/components/recipe/alternate_show.jsx
+++ frontend/src/components/recipe/alternate_show.jsx
@@ -37,6 +37,7 @@ class AlternateShow extends React.Component {
         return (
             <div className="recipe-show-box">This is the Recipe Show 
                 <h1 className="recipe-title">{this.props.recipeObject.title}</h1>
+                <img src={this.props.recipeObject.image} height="100" width="100"></img>
                 <div className="recipe-ingredients">{this.listIngredients()}</div>
                 <p className="recipe-instructions">{this.props.recipeObject.instructions}</p>
                 <div className="save-recipe-div" onClick={this.saveRecipe}>Save Icon</div>
