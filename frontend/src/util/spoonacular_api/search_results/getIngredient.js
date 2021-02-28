af453068da42c98cde5358acd3001a3d2857680c
Tului <tgantulga@ucdavis.edu>
2021-02-28 12:08:10 -0800

    complexSearch is not that useful, stick with recipeByIngredients, added recipeInformationBulk

---
+++ frontend/src/util/spoonacular_api/search_results/getIngredient.js
@@ -0,0 +1,250 @@
+// getIngredient('apple', console.log)
+
+export const getIngredientResult = [
+
+  { id: 9003, name: 'apple', image: 'apple.jpg' },
+  { id: 9019, name: 'applesauce', image: 'applesauce.png' },
+  { id: 9016, name: 'apple juice', image: 'apple-juice.jpg' },
+  { id: 1009016, name: 'apple cider', image: 'apple-cider.jpg' },
+  { id: 10019297, name: 'apple jelly', image: 'apple-jelly.jpg' },
+  { id: 19294, name: 'apple butter', image: 'apple-jelly.jpg' },
+  {
+    id: 1042035,
+    name: 'apple pie spice',
+    image: 'garam-masala.jpg'
+  },
+  {
+    id: 19312,
+    name: 'apple pie filling',
+    image: 'apple-pie-slice.jpg'
+  },
+  {
+    id: 2048,
+    name: 'apple cider vinegar',
+    image: 'apple-cider-vinegar.jpg'
+  },
+  {
+    id: 10123,
+    name: 'applewood smoked bacon',
+    image: 'raw-bacon.png'
+  },
+  { id: 7951, name: 'scrapple', image: 'spam.png' },
+  { id: 9266, name: 'pineapple', image: 'pineapple.jpg' },
+  {
+    id: 1079003,
+    name: 'red apple',
+    image: 'red-delicious-apples.png'
+  },
+  {
+    id: 9273,
+    name: 'pineapple juice',
+    image: 'pineapple-juice.jpg'
+  },
+  {
+    id: 1009354,
+    name: 'pineapple rings',
+    image: 'pineapple-rings.png'
+  },
+  {
+    id: 1029354,
+    name: 'pineapple chunks',
+    image: 'pineapple-with-can.png'
+  },
+  {
+    id: 1039354,
+    name: 'pineapple tidbits',
+    image: 'pineapple-with-can.png'
+  },
+  {
+    id: 9354,
+    name: 'pineapple in juice',
+    image: 'pineapple-with-can.png'
+  },
+  {
+    id: 10219297,
+    name: 'pineapple preserves',
+    image: 'pineapple-preserves.jpg'
+  },
+  {
+    id: 1029003,
+    name: 'tart apple',
+    image: 'grannysmith-apple.png'
+  },
+  { id: 1109003, name: 'gala apple', image: 'apple.jpg' },
+  { id: 1049003, name: 'fuji apple', image: 'apple.jpg' },
+  { id: 99140, name: 'wood apple', image: 'wood-apple.jpg' },
+  { id: 9312, name: 'rose apple', image: 'rose-apples.jpg' },
+  { id: 99171, name: 'star apple', image: 'star-apple.jpg' },
+  {
+    id: 1069003,
+    name: 'green apple',
+    image: 'grannysmith-apple.png'
+  },
+  { id: 9011, name: 'dried apples', image: 'apple-dried.jpg' },
+  { id: 9321, name: 'sugar apple', image: 'sugar-apple.jpg' },
+  {
+    id: 99109,
+    name: 'cashew apple',
+    image: 'cashew-nut-fruit.jpg'
+  },
+  {
+    id: 1019003,
+    name: 'bramley apples',
+    image: 'bramley-apples.png'
+  },
+  { id: 99122, name: 'custard apple', image: 'custard-apple.jpg' },
+  {
+    id: 1009003,
+    name: 'braeburn apple',
+    image: 'braeburn-apples.png'
+  },
+  { id: 1039003, name: 'mcintosh apple', image: 'apple.jpg' },
+  { id: 1099003, name: 'pink lady apple', image: 'apple.jpg' },
+  {
+    id: 93639,
+    name: 'dried pineapple',
+    image: 'candied-ginger-or-crystallized-ginger.jpg'
+  },
+  {
+    id: 1019354,
+    name: 'crushed pineapple',
+    image: 'pineapple-with-can.png'
+  },
+  {
+    id: 1089003,
+    name: 'grannysmith apple',
+    image: 'grannysmith-apple.png'
+  },
+  {
+    id: 1059003,
+    name: 'red delicious apples',
+    image: 'red-delicious-apples.png'
+  },
+  {
+    id: 9501,
+    name: 'golden delicious apple',
+    image: 'apple-golden-delicious.jpg'
+  }]
+[{ id: 9003, name: 'apple', image: 'apple.jpg' },
+{ id: 9019, name: 'applesauce', image: 'applesauce.png' },
+{ id: 9016, name: 'apple juice', image: 'apple-juice.jpg' },
+{ id: 1009016, name: 'apple cider', image: 'apple-cider.jpg' },
+{ id: 10019297, name: 'apple jelly', image: 'apple-jelly.jpg' },
+{ id: 19294, name: 'apple butter', image: 'apple-jelly.jpg' },
+{
+  id: 1042035,
+  name: 'apple pie spice',
+  image: 'garam-masala.jpg'
+},
+{
+  id: 19312,
+  name: 'apple pie filling',
+  image: 'apple-pie-slice.jpg'
+},
+{
+  id: 2048,
+  name: 'apple cider vinegar',
+  image: 'apple-cider-vinegar.jpg'
+},
+{
+  id: 10123,
+  name: 'applewood smoked bacon',
+  image: 'raw-bacon.png'
+},
+{ id: 7951, name: 'scrapple', image: 'spam.png' },
+{ id: 9266, name: 'pineapple', image: 'pineapple.jpg' },
+{
+  id: 1079003,
+  name: 'red apple',
+  image: 'red-delicious-apples.png'
+},
+{
+  id: 9273,
+  name: 'pineapple juice',
+  image: 'pineapple-juice.jpg'
+},
+{
+  id: 1009354,
+  name: 'pineapple rings',
+  image: 'pineapple-rings.png'
+},
+{
+  id: 1029354,
+  name: 'pineapple chunks',
+  image: 'pineapple-with-can.png'
+},
+{
+  id: 1039354,
+  name: 'pineapple tidbits',
+  image: 'pineapple-with-can.png'
+},
+{
+  id: 9354,
+  name: 'pineapple in juice',
+  image: 'pineapple-with-can.png'
+},
+{
+  id: 10219297,
+  name: 'pineapple preserves',
+  image: 'pineapple-preserves.jpg'
+},
+{
+  id: 1029003,
+  name: 'tart apple',
+  image: 'grannysmith-apple.png'
+},
+{ id: 1109003, name: 'gala apple', image: 'apple.jpg' },
+{ id: 1049003, name: 'fuji apple', image: 'apple.jpg' },
+{ id: 99140, name: 'wood apple', image: 'wood-apple.jpg' },
+{ id: 9312, name: 'rose apple', image: 'rose-apples.jpg' },
+{ id: 99171, name: 'star apple', image: 'star-apple.jpg' },
+{
+  id: 1069003,
+  name: 'green apple',
+  image: 'grannysmith-apple.png'
+},
+{ id: 9011, name: 'dried apples', image: 'apple-dried.jpg' },
+{ id: 9321, name: 'sugar apple', image: 'sugar-apple.jpg' },
+{
+  id: 99109,
+  name: 'cashew apple',
+  image: 'cashew-nut-fruit.jpg'
+},
+{
+  id: 1019003,
+  name: 'bramley apples',
+  image: 'bramley-apples.png'
+},
+{ id: 99122, name: 'custard apple', image: 'custard-apple.jpg' },
+{
+  id: 1009003,
+  name: 'braeburn apple',
+  image: 'braeburn-apples.png'
+},
+{ id: 1039003, name: 'mcintosh apple', image: 'apple.jpg' },
+{ id: 1099003, name: 'pink lady apple', image: 'apple.jpg' },
+{
+  id: 93639,
+  name: 'dried pineapple',
+  image: 'candied-ginger-or-crystallized-ginger.jpg'
+},
+{
+  id: 1019354,
+  name: 'crushed pineapple',
+  image: 'pineapple-with-can.png'
+},
+{
+  id: 1089003,
+  name: 'grannysmith apple',
+  image: 'grannysmith-apple.png'
+},
+{
+  id: 1059003,
+  name: 'red delicious apples',
+  image: 'red-delicious-apples.png'
+},
+  {
+    id: 9501,
+    name: 'golden delicious apple',
+    image: 'apple-golden-delicious.jpg'
+  }]
--- frontend/src/util/spoonacular_api/spoonacular_api.js
+++ frontend/src/util/spoonacular_api/spoonacular_api.js
@@ -1,23 +1,23 @@
 const axios = require('axios');
+const { get } = require('mongoose');
 const spoonacular = require('./keys');
 // const apiKeyToUse = spoonacular["apiKey" + Math.floor((Math.random() * 4) + 1)] //uses random apiKey between 1 and 4
-const apiKeyToUse = spoonacular.apiKey5
+const apiKeyToUse = spoonacular.apiKey1
 
 
 // 01. A function that gets ingredients
 const getIngredient = (query, printToFileFunction) => axios.get(
   spoonacular.getIngredientsURL,
   {
-  params: { // params is the body parameters
-    query,
-    apiKey: apiKeyToUse,
-    number: 100 //100 is the maximum
-  }
+    params: { // params is the body parameters
+      query,
+      apiKey: apiKeyToUse,
+      number: 100 //100 is the maximum
+    }
 
-})
+  })
   .then(payload => {
     console.log(payload.data.results)
-    // debugger
     printToFileFunction(payload.data.results)
   })
 
@@ -27,7 +27,7 @@ const getIngredient = (query, printToFileFunction) => axios.get(
 
 
 // 02. A function that gets recipes given ingredients
-const getRecipeByIngredients = (ingredients, cbFunction, recipeCount=15) => axios.get(
+const getRecipeByIngredients = (ingredients, cbFunction, recipeCount = 15) => axios.get(
   spoonacular.getRecipesByIngredientsURL,
   {
     params: { // params is the body parameters
@@ -46,173 +46,52 @@ const getRecipeByIngredients = (ingredients, cbFunction, recipeCount=15) => axio
 // 03. A function that gets information on a particular recipe
 const getRecipeInformation = (id, cbFunction) => axios.get(
   spoonacular.getRecipeInformationURL(id),
-  { params: {
-    apiKey: apiKeyToUse,
-  }}
+  {
+    params: {
+      apiKey: apiKeyToUse,
+    }
+  })
 
-
-).then(payload => cbFunction(payload.data)) // we're gonna need to dispatch this through our reducers and middleware)
+  .then(payload => cbFunction(payload.data)) // we're gonna need to dispatch this through our reducers and middleware)
   .catch(err => console.log(err))
 
 
-// getRecipeInformation(45641)
+// 04. A function that gets information on multiple recipes
+const getRecipeInformationBulk = (ids, cbFunction) => axios.get(
+  spoonacular.getRecipeInformationBulkURL,
+  {
+    params: {
+      apiKey: apiKeyToUse,
+      ids
+    }
+  })
+
+  .then(payload => cbFunction(payload.data)) // we're gonna need to dispatch this through our reducers and middleware)
+  .catch(err => console.log(err))
+
+
+
+
+
+
 
 
 // Exporting 3 functions that were created
 module.exports = {
   getIngredient,
   getRecipeByIngredients,
-  getRecipeInformation
+  getRecipeInformation,
+  getRecipeInformationBulk
+
 }
 
 
+// Below are sample commands you can run to see output, they are also located in search_results/
 // getRecipeByIngredients('apple,blueberry', console.log)
 // getRecipeInformation(9003, console.log)
-// getIngredient('apple')
+// getIngredient('apple', console.log)
+getRecipeInformationBulk('661322,643857', console.log)
 
 
 
 
-
-
-
-
-// {
-//   id: 423958,
-//     title: 'Fruit Salad with O.J. Reduction',
-//       image: 'https://spoonacular.com/recipeImages/423958-312x231.jpg',
-//         imageType: 'jpg',
-//           usedIngredientCount: 3,
-//             missedIngredientCount: 4,
-//               missedIngredients:
-//   [{
-//     id: 9152,
-//     amount: 2,
-//     unit: 'tablespoons',
-//     unitLong: 'tablespoons',
-//     unitShort: 'Tbsp',
-//     aisle: 'Produce',
-//     name: 'lemon juice',
-//     original: '2 tablespoons lemon juice',
-//     originalString: '2 tablespoons lemon juice',
-//     originalName: 'lemon juice',
-//     metaInformation: [],
-//     meta: [],
-//     image:
-//       'https://spoonacular.com/cdn/ingredients_100x100/lemon-juice.jpg'
-//   },
-//   {
-//     id: 9202,
-//     amount: 2,
-//     unit: 'medium',
-//     unitLong: 'mediums',
-//     unitShort: 'medium',
-//     aisle: 'Produce',
-//     name: 'navel oranges',
-//     original: '2 medium navel oranges, peeled, sectioned and chopped',
-//     originalString: '2 medium navel oranges, peeled, sectioned and chopped',
-//     originalName: 'navel oranges, peeled, sectioned and chopped',
-//     metaInformation: [Array],
-//     meta: [Array],
-//     image:
-//       'https://spoonacular.com/cdn/ingredients_100x100/mandarin-or-tangerines-or-clementines.jpg'
-//   },
-//   {
-//     id: 9206,
-//     amount: 1,
-//     unit: 'cup',
-//     unitLong: 'cup',
-//     unitShort: 'cup',
-//     aisle: 'Beverages',
-//     name: 'orange juice',
-//     original: '1 cup orange juice',
-//     originalString: '1 cup orange juice',
-//     originalName: 'orange juice',
-//     metaInformation: [],
-//     meta: [],
-//     image:
-//       'https://spoonacular.com/cdn/ingredients_100x100/orange-juice.jpg'
-//   },
-//   {
-//     id: 9236,
-//     amount: 2,
-//     unit: 'medium',
-//     unitLong: 'mediums',
-//     unitShort: 'medium',
-//     aisle: 'Produce',
-//     name: 'peaches',
-//     original: '2 medium peaches, cubed',
-//     originalString: '2 medium peaches, cubed',
-//     originalName: 'peaches, cubed',
-//     metaInformation: [Array],
-//     meta: [Array],
-//     image: 'https://spoonacular.com/cdn/ingredients_100x100/peach.png'
-//   }],
-//     usedIngredients:
-//   [{
-//     id: 9050,
-//     amount: 2,
-//     unit: 'cups',
-//     unitLong: 'cups',
-//     unitShort: 'cup',
-//     aisle: 'Produce',
-//     name: 'blueberries',
-//     original: '2 cups fresh blueberries',
-//     originalString: '2 cups fresh blueberries',
-//     originalName: 'fresh blueberries',
-//     metaInformation: [Array],
-//     meta: [Array],
-//     extendedName: 'fresh blueberries',
-//     image:
-//       'https://spoonacular.com/cdn/ingredients_100x100/blueberries.jpg'
-//   },
-//   {
-//     id: 1069003,
-//     amount: 1,
-//     unit: 'medium',
-//     unitLong: 'medium',
-//     unitShort: 'medium',
-//     aisle: 'Produce',
-//     name: 'green apple',
-//     original: '1 medium green apple, cubed',
-//     originalString: '1 medium green apple, cubed',
-//     originalName: 'green apple, cubed',
-//     metaInformation: [Array],
-//     meta: [Array],
-//     image:
-//       'https://spoonacular.com/cdn/ingredients_100x100/grannysmith-apple.png'
-//   },
-//   {
-//     id: 1079003,
-//     amount: 2,
-//     unit: 'medium',
-//     unitLong: 'mediums',
-//     unitShort: 'medium',
-//     aisle: 'Produce',
-//     name: 'red apples',
-//     original: '2 medium red apples, cubed',
-//     originalString: '2 medium red apples, cubed',
-//     originalName: 'red apples, cubed',
-//     metaInformation: [Array],
-//     meta: [Array],
-//     image:
-//       'https://spoonacular.com/cdn/ingredients_100x100/red-delicious-apples.png'
-//   }],
-//     unusedIngredients:
-//   [{
-//     id: 9003,
-//     amount: 1,
-//     unit: 'serving',
-//     unitLong: 'serving',
-//     unitShort: 'serving',
-//     aisle: 'Produce',
-//     name: 'apple',
-//     original: 'apple',
-//     originalString: 'apple',
-//     originalName: 'apple',
-//     metaInformation: [],
-//     meta: [],
-//     image: 'https://spoonacular.com/cdn/ingredients_100x100/apple.jpg'
-//   }],
-//     likes: 0
-// }
