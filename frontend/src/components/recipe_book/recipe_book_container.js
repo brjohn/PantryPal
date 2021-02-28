072afdd13c317a86ab5685d895b02c38715ff5f9
Tului <tgantulga@ucdavis.edu>
2021-02-27 19:47:43 -0800

    deleting saved_recipes and updating database works

--- frontend/src/components/recipe_book/recipe_book_container.js
+++ frontend/src/components/recipe_book/recipe_book_container.js
@@ -1,17 +1,14 @@
 import { connect } from 'react-redux';
-import { fetchUser, updateUser } from '../../actions/user_actions';
+import { updateUser } from '../../actions/user_actions';
 import RecipeBook from './recipe_book';
 
 const mapStateToProps = (state) => {
-  // debugger
   return {
     currentUser: state.session.currentUser,
-    // saved_recipes: state.users.recipes
   }
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchUser: id => dispatch(fetchUser(id)),
   updateUser: (data) => dispatch(updateUser(data))
 });
 
