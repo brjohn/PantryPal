4e68db9a0568a3d9b23b650ec55659b712adc3b5
Tului <tgantulga@ucdavis.edu>
2021-02-28 19:15:46 -0800

    fixed users_reducer, rerenders properly now

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -1,17 +1,18 @@
-import {RECEIVE_CURRENT_USER} from '../actions/session_actions';
-import {RECEIVE_USER} from '../actions/user_actions';
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_USER } from '../actions/user_actions';
 
 const usersReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
-    switch(action.type){
+    switch (action.type) {
         case RECEIVE_CURRENT_USER:
             return Object.assign({}, oldState, action.currentUser);
         case RECEIVE_USER:
             // debugger
-            return Object.assign({}, action.user.data );
+            return Object.assign({}, action.user.data);
         default:
             return oldState;
     }
 };
+// test
 
 export default usersReducer;
