ef404e6e90b0e63361736ad13598c7d8c50ec0bf
Tului <tgantulga@ucdavis.edu>
2021-03-15 10:51:49 -0700

    added a few actions for users

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -3,27 +3,66 @@ import { receiveCurrentUser } from './session_actions';
 
 
 
-
 export const RECEIVE_USER = "RECEIVE_USER";
-// export const RECEIVE_USER_ERRORS = "RECEIVE_USER_ERRORS";
-// export const CLEAR_USER_ERRORS = "CLEAR_USER_ERRORS";
+export const RECEIVE_PREFERENCES = "RECEIVE_PREFERENCES";
+export const RECEIVE_EXCLUSIONS = "RECEIVE_EXCLUSIONS";
+export const RECEIVE_INGREDIENTS = "RECEIVE_INGREDIENTS";
+export const RECEIVE_RECIPES = "RECEIVE_RECIPES";
+export const RECEIVE_SAVED_RECIPES = "RECEIVE_SAVED_RECIPES";
 
-const receiveUser = (user) => ({
-    type: RECEIVE_USER,
-    user
+
+// this needs to go away since we're getting rid of users
+const receiveUser = user => ({
+  type: RECEIVE_USER,
+  user
 });
 
+const receivePreferences = preferences => ({
+  type: RECEIVE_PREFERENCES,
+  preferences
+})
+
+const receiveExclusions = exclusions => ({
+  type: RECEIVE_EXCLUSIONS,
+  exclusions
+})
+
+const receiveIngredients = ingredients => ({
+  type: RECEIVE_INGREDIENTS,
+  ingredients
+})
+
+const receiveRecipes = recipes => ({
+  type: RECEIVE_RECIPES,
+  recipes
+})
+
+const receiveSavedRecipes = saved_recipes => ({
+  type: RECEIVE_SAVED_RECIPES,
+  saved_recipes
+})
+
+
+
+
+
+
+
+
+
+
+
+
 export const fetchUser = (userId) => (dispatch) => (
-    UserApiUtil.fetchUser(userId).then(user => dispatch(receiveUser(user)))
+  UserApiUtil.fetchUser(userId).then(user => dispatch(receiveUser(user)))
 )
 
 export const fetchUserRefresh = (userId) => (dispatch) => (
-    UserApiUtil.fetchUser(userId).then(user => {
-        
-        // debugger
-        dispatch(receiveCurrentUser(user.data))})
+  UserApiUtil.fetchUser(userId).then(user => {
+    dispatch(receiveCurrentUser(user.data))
+  })
 )
 
 export const updateUser = (data) => (dispatch) => (
-    UserApiUtil.updateUser(data).then(newUser => dispatch(receiveUser(newUser)))
+  UserApiUtil.updateUser(data).then(newUser => dispatch(receiveUser(newUser)))
 )
